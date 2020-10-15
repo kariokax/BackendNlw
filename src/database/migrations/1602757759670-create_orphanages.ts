35531f4b48679e52216e622423cb6734809cabdc
kariokax <felipe.c.machado123@gmail.com>
2020-10-15 17:07:17 -0300

    criação models e ajustes na migration e configs

--- src/database/migrations/1602757759670-create_orphanages.ts
+++ src/database/migrations/1602757759670-create_orphanages.ts
@@ -42,6 +42,10 @@ export class createOrphanages1602757759670 implements MigrationInterface {
                 name:'open_on_weekends',
                 type:'boolean',
                 default: false  
+              },
+              {
+                  name:'opening_hours',
+                  type:'varchar'
               }
             ]
         }));
