c8e22bc44e0aa15b3b5edc732acd0224b75224e3
kariokax <felipe.c.machado123@gmail.com>
2020-10-15 09:28:00 -0300

    Configuração banco e criação da migration

---
+++ src/database/migrations/1602757759670-create_orphanages.ts
@@ -0,0 +1,54 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createOrphanages1602757759670 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name:'orphanages',
+            columns:[
+              {
+                name: 'id',
+                type:'integer',
+                unsigned: true,//coluna não pode ser negativa, sempre será um número positivo
+                isPrimary:true,
+                isGenerated:true,//coluna gerada automaticamente
+                generationStrategy:'increment',//autoincrement
+              },
+              {
+                  name: 'name',
+                  type: 'varchar'
+              },
+              {
+                  name:'latitude',
+                  type:'decimal',
+                  scale:10, //max numero apos a virgula
+                  precision:2,//max numero antes da virgula
+              },
+              {
+                name:'longitude',
+                type:'decimal',
+                scale:10,
+                precision:2,
+              },
+              {
+                name:'about',
+                type:'text'  
+              },
+              {
+                name:'instructions',
+                type:'text'  
+              },
+              {
+                name:'open_on_weekends',
+                type:'boolean',
+                default: false  
+              }
+            ]
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('orphanages');
+    }
+
+}
